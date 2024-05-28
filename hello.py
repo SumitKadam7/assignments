b77bb0ca801f01ce0a141cd5b96733519488e749
suraj <surajkadam50@gmail.com>
2024-05-28 21:45:32 +0530

    calculator

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
-print ("helloword")
-print ("sumit")
+number1 = input('enter the num ')
+num1 = int (number1)
+sign = input ('')
+number2 = input ('enter the number ')
+num2 = int(number2)
+if sign == '+':
+    print (num1+num2)
+elif sign == '-':
+    print (num1-num2)
+elif sign == '*':
+    print (num1*num2)
+elif sign == '/':
+    print (num1/num2)
+else:
+    print ('enter + , - , * or /')
+
+
