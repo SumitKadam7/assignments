77733bb2de26ae2edf46e832b1db10925b0da95e
Suraj.Kadam7 <surajskadam7@gmail.com>
2024-06-01 23:06:53 +0530

    questions

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 n=int(input("enter the number of rows:"))
 num=1
-for row in range (1+n+1):
-    for col in range (1,row+1):
-        print(num,end=" ")
+for row in range (1, n+1):
+    for col in range (row):
+        print(num, end=" ")
         num=num+1
      
-    print()
+    print("")
 
-    
+
+
