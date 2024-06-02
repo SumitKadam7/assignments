f3f64fc688c3e78c1a54979a82cde9621f929172
suraj <surajkadam50@gmail.com>
2024-06-02 20:14:02 +0530

    odd number right angle

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
-n=int(input("enter the number of rows:"))
+n=int(input("enter the number of rows: "))
 num=1
-for row in range (1, n+1):
-    for col in range (row):
-        print(num, end=" ")
-        num=num+1
-     
-    print("")
-
-
-
+for row in range(1,n+1):
+    for col in range(1,row+1):
+        print (num,end=" ")
+        num+=2
+    print()
