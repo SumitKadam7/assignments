08f0cdd40539a4c612b72a683415984ee2bc5f58
suraj <surajkadam50@gmail.com>
2024-06-04 21:21:38 +0530

    list solution

--- list.py
+++ list.py
@@ -1,9 +1,9 @@
 # q1 
 # [1,2,3,5,0,9]
-list[1,2,3,5,0,9]
+list=[1,2,3,5,0,9]
 n = int(input("enter a numer in list : "))
-
-for i in range (6):
+print("length of list",len(list))
+for i in range (len(list)):
     if n == list [i]:
         print("number present in list at position ", i)
 
